090f3749e102c5331136298356d543c8b4e8a9a5
chadfraser <32744531+chadfraser@users.noreply.github.com>
2018-05-26 06:13:09 -0600

    Remove a now-unused line of code accessing a spritesheet

--- game/tools/window_icon.py
+++ game/tools/window_icon.py
@@ -1,11 +1,9 @@
 import pygame as pg
 import os
 
-from game.sprites.sprite_sheet import SpriteSheet
 import game.tools.constants as c
 
 
-ICON_SHEET = SpriteSheet("display.png")
 ICON = pg.image.load(os.path.join(c.RESOURCE_FOLDER, "game_display_icon.png")).convert()
 ICON.set_colorkey(c.GREEN)
 pg.display.set_icon(ICON)
